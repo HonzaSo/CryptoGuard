33c285fd1f3608943f565ac29f63c36d47f979e3
HonzaSo <jan.sobolciak@gmail.com>
2026-04-13 17:23:47 +0200

    remove duplicate code

--- CryptoGuard.Infrastructure/Entities/Configurations/AssetConfiguration.cs
+++ CryptoGuard.Infrastructure/Entities/Configurations/AssetConfiguration.cs
@@ -12,13 +12,12 @@ public class AssetConfiguration : IEntityTypeConfiguration<AssetEntity>
         builder.Property(a => a.Symbol).IsRequired().HasMaxLength(10);
         builder.Property(a => a.Name).HasMaxLength(100);
         builder.Property(a => a.CurrentPrice).HasColumnType("decimal(18,8)");
-        builder.Property(a => a.Currency).IsRequired().HasMaxLength(3);
         builder.Property(a => a.Currency)
             .IsRequired()
             .HasMaxLength(3)
             .HasConversion(
                 c => c.Code, 
-                s => Currency.Create(s).Value
+                s => Currency.Create(s).Value!
             );
         
         builder.HasIndex(a => a.Symbol).IsUnique();
